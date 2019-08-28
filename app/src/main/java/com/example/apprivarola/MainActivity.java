16cfb6cc1a5e9674a1c6cecd8a9cb59bf0e576de
gustavo rivarola <gusguz1994@gmail.com>
2019-08-28 18:48:40 -0300

    ya estaria

--- app/src/main/java/com/example/apprivarola/MainActivity.java
+++ app/src/main/java/com/example/apprivarola/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.apprivarola;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private AvisoMensaje am;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        this.am = new AvisoMensaje();
+        registerReceiver(this.am, new IntentFilter("android.provider.Telephony.SMS_RECEIVED"));
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(am);
     }
 }
